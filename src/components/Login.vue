f9225eb66d010a8eaedcaead890c27e5e65a5e99
zhanghaoran <zhr5898@qq.com>
2020-12-12 14:38:03 +0800

    增加一些配置

---
+++ src/components/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h4>这是登录组件</h4>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.config.productionTip = false
+// Nprogress.set(0.6)
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  // Nprogress.inc()
+  Nprogress.start()
+  next()
+})
+
+router.afterEach(() => {
+  Nprogress.done()
+})
 
 new Vue({
   router,
